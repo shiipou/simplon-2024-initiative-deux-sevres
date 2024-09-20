bf17c7ecced514c4661beca18440ffbcad1d3d93
CADET Flavien <flavien.cadet@externe.maif.fr>
2024-09-20 09:17:25 +0200

    feat!: Initalisation Springboot

---
+++ src/main/java/fr/initiativedeuxsevres/ttm/TrouveTonMatchApplication.java
@@ -0,0 +1,13 @@
+package fr.initiativedeuxsevres.ttm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrouveTonMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrouveTonMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/initiativedeuxsevres/ttm/TrouveTonMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.initiativedeuxsevres.ttm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrouveTonMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
